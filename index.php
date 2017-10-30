fa92773cfdc7b45e4985e6c95c16557e584b6dc5
d-salt <inai.yuma@gmail.com>
2017-10-30 23:02:57 +0900

    path

--- index.php
+++ index.php
@@ -63,8 +63,8 @@
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<!-- <script src="js/jquery-3.2.1.min.js"></script> -->
-	<script src="js/jquery.cookie.js"></script>
-	<script src="js/jquery.layerBoard.js"></script>
+	<script src="/js/jquery.cookie.js"></script>
+	<script src="/js/jquery.layerBoard.js"></script>
 <!-- 	<script>
 		$(function(){
  
