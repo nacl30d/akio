3105f23a99888b5c1d0378fdd3e2fb13bf0efbc9
d-salt <inai.yuma@gmail.com>
2017-10-30 22:49:50 +0900

    jquery

--- index.php
+++ index.php
@@ -60,6 +60,7 @@
 
 	<!-- 2017-10-30 -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/jquery.cookie.js"></script>
 	<script src="js/jquery.layerBoard.js"></script>
 	<script>
