b869215dc2bbf2591bc8968a6418d3e29b90d7f5
ShiozawaDaiki <d-salt@SaltBook-Pro.local>
2017-12-22 16:35:28 +0900

    small fixed

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <footer class="page-footer green">
     <div class="container">
       <div class="row">
-        <div class="col l3 s12">
+        <div class="col l6 s12">
           <h5 class="white-text">D.Salt</h5>
           <p class="grey-text text-lighten-4">This tool made for University Students. I hope your campus life will be better.</p>
         </div>
