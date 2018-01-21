38a66ca60fa6dc94ae10ca5c4a324ad4cb1b344e
ShiozawaDaiki <d-salt@SaltBook-Pro.local>
2018-01-21 15:24:52 +0900

    db connection info changed

--- conf/config.php
+++ conf/config.php
@@ -1,10 +1,35 @@
 <?php
 // データベース接続に必要な情報
-    $host = "127.0.0.1";
-    $port = "55930";
-    $db_name ="localdb";
-    $db_user = "azure";
-    $db_passwd = "6#vWHD_$";
+$host = '';
+$db_name = '';
+$db_user = '';
+$db_passwd = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $host = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $db_name = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $db_user = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $db_passwd = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$link = mysqli_connect($host, $db_user, $db_passwd,$db_name);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+
 
 // リダイレクトする際の自サイトの情報
     $url = "http://akico.azurewebsites.net/";
